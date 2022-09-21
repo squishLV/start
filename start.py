b458292cc53cd8986510d8b8029c17bfcc774bd8
squishLV <114050812+squishLV@users.noreply.github.com>
2022-09-21 10:33:39 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+age = 16
+avarage_grade = 10
+
+if avarage_grade < 0 or avarage_grade > 10:
+  print('error')
+elif avarage_grade < 6:
+  print('piedodiet, jus neesat pienemts')
+elif avarage_grade < 9:
+  print('apsveicu, jus esat pienemts uz maksas vietu')
+else:
+  print('apsveicu, jus esat pienemts uz bugeta vietu')
